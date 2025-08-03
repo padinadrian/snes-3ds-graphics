7c311fe60f9a2b72d6ea66a1bb5a21dced10df37
Adrian Padin <padin.adrian@gmail.com>
2025-08-03 17:13:46 -0400

    Function to convert snes palette to RGBA format

--- source/palette.h
+++ source/palette.h
@@ -4,6 +4,13 @@
 #include <stdint.h>
 
 enum { NUM_CGRAM_COLORS = 256 };
+enum { CGRAM_SIZE = NUM_CGRAM_COLORS * 2 };
+
+enum {
+    PALETTE_RED_MASK   = 0b11111,
+    PALETTE_GREEN_MASK = PALETTE_RED_MASK << 5,
+    PALETTE_BLUE_MASK  = PALETTE_GREEN_MASK << 5,
+};
 
 typedef struct {
     // Red component of color
@@ -42,4 +49,22 @@ static inline uint8_t palette_color_get_green(PaletteColor color) {
  */
 static inline uint8_t palette_color_get_blue(PaletteColor color) {
     return color.blue;
+}
+
+/**
+ * @brief Convert a 5-bit RGB color to an 8-bit RGBA color.
+ */
+static inline uint32_t convert_palette_to_rgba(uint16_t color)
+{
+    uint32_t color_int = (uint32_t)color;
+    uint32_t red_int   = color_int & PALETTE_RED_MASK;
+    uint32_t green_int = color_int & PALETTE_GREEN_MASK;
+    uint32_t blue_int  = color_int & PALETTE_BLUE_MASK;
+
+    // Red:   color >> 0 << 24 << 3 = color << 27
+    // Green: color >> 5 << 16 << 3 = color << 14
+    // Blue:  color >> 10 << 8 << 3 = color << 1
+
+    // Return as RGBA format
+    return (red_int << 27) | (green_int << 14) | (blue_int << 1) | 0xFF;
 }
--- tests/palette_tests.cpp
+++ tests/palette_tests.cpp
@@ -29,4 +29,22 @@ TEST(PaletteTest, PaletteColorGetColor2)
     EXPECT_EQ(palette_color_get_blue(color), 28);
 }
 
+TEST(PaletteTest, ConvertPaletteToRgba1)
+{
+    // Blue = 31
+    // Green = 31
+    // Red = 31
+    uint16_t color = 0b0111111111111111;
+    EXPECT_EQ(convert_palette_to_rgba(color), 0xF8F8F8FF);
+}
+
+TEST(PaletteTest, ConvertPaletteToRgba2)
+{
+    // Blue = 5
+    // Green = 10
+    // Red = 15
+    uint16_t color = 0b0001010101001111;
+    EXPECT_EQ(convert_palette_to_rgba(color), 0x785028FF);
+}
+
 }   // namespace
