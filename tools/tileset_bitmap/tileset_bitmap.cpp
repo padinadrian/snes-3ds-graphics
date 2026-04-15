26161efe03babe05b1c04ac9ff9e43d6eb17fd26
Adrian Padin <padin.adrian@gmail.com>
2026-04-14 22:36:10 -0400

    feat: Decode 2bpp and 8bpp in tileset_bitmap

--- tools/tileset_bitmap/tileset_bitmap.cpp
+++ tools/tileset_bitmap/tileset_bitmap.cpp
@@ -47,9 +47,11 @@ void copy_to_bitmap(
         uint8_t tile[],
         bitmap_image& image,
         size_t x_offset,
-        size_t y_offset
+        size_t y_offset,
+        int bpp
 )
 {
+    int bit_offset = 8 - bpp;
     for (size_t y = 0; y < 8; y += 1)
     {
         for (size_t x = 0; x < 8; x += 1)
@@ -57,13 +59,12 @@ void copy_to_bitmap(
             const size_t index = (y * 8) + x;
             size_t x_final = x_offset + x;
             size_t y_final = y_offset + y;
-            printf("tile: x: %lu;\ty: %lu;\tindex: %lu\n", x_final, y_final, index);
             image.set_pixel(
                 x_final,
                 y_final,
-                tile[index] << 4,
-                tile[index] << 4,
-                tile[index] << 4
+                tile[index] << bit_offset,
+                tile[index] << bit_offset,
+                tile[index] << bit_offset
             );
         }
     }
@@ -75,31 +76,52 @@ void copy_to_bitmap(
  * @note For now this is hardcoded to work for 4bpp. Other modes will be
  * supported in the future.
  */
-void generate_tileset_bitmap(uint16_t* vram, const char* output_file)
+void generate_tileset_bitmap(uint16_t* vram, int bpp, const char* output_file)
 {
-    // Tile* tileset = (Tile*)(&vram[0]);
+    // Settings
+    size_t vram_step = 0;
+    void (*tile_decode_fn)(uint8_t*, const uint16_t*);
 
-    const size_t width = 128;   // 16 columns of 8x8 tiles = 128 pixels wide
-    const size_t height = 1024; // 64 rows of 8x8 tiles = 512 pixels tall
+    if (bpp == 2)
+    {
+        vram_step = 8;
+        tile_decode_fn = tile_decode_2bpp;
+    }
+    else if (bpp == 4)
+    {
+        vram_step = 16;
+        tile_decode_fn = tile_decode_4bpp;
+    }
+    else if (bpp == 8)
+    {
+        vram_step = 32;
+        tile_decode_fn = tile_decode_8bpp;
+    }
+    else
+    {
+        printf("Invalid bpp: %d\n", bpp);
+        return;
+    }
+
+    const uint32_t width = 128;   // 16 columns of 8x8 tiles = 128 pixels wide
+    const uint32_t height = VRAM_COUNT / 8 / bpp; // Number of pixels tall
     bitmap_image image{width, height};
 
     uint8_t tile[64] = {};
 
     // Each tile is 8x8 pixels
-    // printf("vram: x: %lu;\ty: %lu;\tindex: %lu\n", x, y, index);
     size_t vram_offset = 0;
     for (size_t y = 0; y < height; y += 8)
     {
         for (size_t x = 0; x < width; x += 8)
         {
             printf("vram offset; 0x%08lx (%lu)\n", vram_offset, vram_offset);
-            tile_decode_4bpp(tile, &vram[vram_offset]);
-            copy_to_bitmap(tile, image, x, y);
-            memset(tile, 0, 64);
-            vram_offset += 16;
+            tile_decode_fn(tile, &vram[vram_offset]);
+            copy_to_bitmap(tile, image, x, y, bpp);
+            memset(tile, 0, sizeof(tile));
+            vram_offset += vram_step;
         }
     }
-
     image.save_image(output_file);
 }
 
@@ -110,6 +132,17 @@ int main(int argc, char** argv)
     {
         printf("2 arguments required: [input_file] [output_file]\n");
         return 1;
+    }
+
+    int bpp = 4;
+    if (argc > 3)
+    {
+        bpp = argv[3][0] - '0';
+        if (bpp != 2 && bpp != 4 && bpp != 8)
+        {
+            printf("Invalid bpp: %s\n", argv[3]);
+            return 1;
+        }
     }
 
     uint16_t vram[VRAM_COUNT];
@@ -125,5 +158,5 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    generate_tileset_bitmap(vram, argv[2]);
+    generate_tileset_bitmap(vram, bpp, argv[2]);
 }
